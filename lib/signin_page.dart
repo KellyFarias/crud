c20c1fdeca21fa631f8a271e1684053231b69f44
Iván Mancilla <54366051+IvanMancilla@users.noreply.github.com>
2020-10-06 18:34:57 -0500

    SignIn

---
+++ lib/signin_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
