95e642810e833b58c2f26bc478df53c69efa076a
Ojos1618 <54366051+Ojos1618@users.noreply.github.com>
2020-10-06 00:17:37 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
 void main() {
   runApp(MyCrud());
 }
@@ -12,11 +13,7 @@ class MyCrud extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Crud 3',
-      home: LoginPage(title: 'Firebase Auth')
-
-      
-    );
+        title: 'Crud 3', home: LoginPage(title: 'Firebase Auth'));
   }
 }
 
@@ -28,18 +25,17 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController emailController= TextEditingController();
-  TextEditingController passwdController=TextEditingController();
-  
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwdController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Container(
+        appBar: AppBar(
+          title: Text('Login Page'),
+        ),
+        body: Container(
           padding: EdgeInsets.all(15.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -47,16 +43,25 @@ class _LoginPageState extends State<LoginPage> {
               TextField(
                 controller: emailController,
                 decoration: InputDecoration(
-                  labelText: 'Email',
-                  icon: Icon(Icons.email)
-                ),
+                    labelText: 'Email', icon: Icon(Icons.email)),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              TextField(
+                controller: passwdController,
+                decoration: InputDecoration(
+                    labelText: 'Password', icon: Icon(Icons.vpn_key)),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              FlatButton(
+                child: Text('Login'),
+                color: Colors.orangeAccent,
               )
-              
             ],
           ),
-        )
-    );
+        ));
   }
 }
-
-
